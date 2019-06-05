852f268a9c093784e5e35a224191b6ba331d8a34
Dinah Chen <Dinah>
2019-06-05 15:31:21 -0700

    second commit

--- custom-jq.js
+++ custom-jq.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
     $('#jq-options').addClass('select-hide')
-    $('.custom-select-jq select option').hide();
+    $('.custom-select-jq select').hide();
+    $('.custom-select-jq').append("<div id='options'></div>");
+    var defaultText = "hello";
+    $('#options').html(defaultText);
     $('.custom-select-jq').click(function(){
         $(this).toggleClass('select-half');
         $('#jq-options').toggleClass('select-hide');
