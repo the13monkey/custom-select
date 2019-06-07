c6da985f7a7f90a888bb1906bba5a6c418bf5ed4
Dinah Chen <dinah.chen.ccy@gmail.com>
2019-06-06 21:42:37 -0700

    first final upload

--- custom-jq.js
+++ custom-jq.js
@@ -1,12 +1,25 @@
 $( document ).ready(function() {
+
     $('#jq-options').addClass('select-hide')
     $('.custom-select-jq select').hide();
     $('.custom-select-jq').append("<div id='options'></div>");
-    var defaultText = "hello";
+    var defaultText = "Select an option below:";
     $('#options').html(defaultText);
-    $('.custom-select-jq').click(function(){
+    $('.custom-select-jq').click(function(e){
+        e.stopPropagation();
         $(this).toggleClass('select-half');
         $('#jq-options').toggleClass('select-hide');
     });
+    $('#jq-options li').click(function(){
+        $('.jq-selected').removeClass('jq-selected');
+        $(this).addClass('jq-selected');
+        var data = $(this).html();
+        $('#options').html(data);
+    });
+    $(document).click(function(){
+        $("#jq-options").hide();
+        $('.custom-select-jq').removeClass('select-half');
+    });
+      
     
 });
